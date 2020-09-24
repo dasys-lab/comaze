48f198c9caae00e541ee5e05ea8637b101225742
Kai Liebscher <kliebscher@micromata.de>
2020-09-24 13:05:55 +0200

    spring initializr

---
+++ src/main/java/de/unikassel/vs/magicmaze/MagicMazeGameServerApplication.java
@@ -0,0 +1,13 @@
+package de.unikassel.vs.magicmaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicMazeGameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicMazeGameServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/unikassel/vs/magicmaze/MagicMazeGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.unikassel.vs.magicmaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicMazeGameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

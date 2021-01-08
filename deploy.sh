a1ebe9bb36f01fb5a6e14f11ba0f73ab903872b5
Kai Liebscher <kai-l@gmx.de>
2021-01-08 10:37:09 +0100

    wait between killing the running server and starting the new one

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn clean package && ssh comaze 'rm comaze-server/*' && scp target/comaze*.jar comaze:comaze-server/ && ssh comaze 'screen -XS comaze-server quit'; ssh comaze 'screen -dmS comaze-server java -jar comaze-server/*jar'
+mvn clean package && ssh comaze 'rm comaze-server/*' && scp target/comaze*.jar comaze:comaze-server/ && ssh comaze 'screen -XS comaze-server quit'; sleep 5; ssh comaze 'screen -dmS comaze-server java -jar comaze-server/*jar'

0a6b394dbb098e1614fb2ae17dfa3baf742b1340
Kai Liebscher <kai-l@gmx.de>
2021-01-10 23:08:34 +0100

    actively wait for port to be free before restarting server

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-mvn clean package && ssh comaze 'rm comaze-server/*' && scp target/comaze*.jar comaze:comaze-server/ && ssh comaze 'screen -XS comaze-server quit'; sleep 5; ssh comaze 'screen -dmS comaze-server java -jar comaze-server/*jar'
+mvn clean package\
+&& ssh comaze 'rm comaze-server/*' \
+&& scp target/comaze*.jar comaze:comaze-server/ \
+&& ssh comaze 'screen -XS comaze-server quit'; \
+echo 'Waiting for server to shut down...'; \
+ssh comaze 'while nc -z localhost 16216; do  sleep 0.1; done'; \
+echo 'Restarting server...'; \
+ssh comaze 'screen -dmS comaze-server java -jar comaze-server/*jar'
